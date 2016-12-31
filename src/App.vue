d07969538bc911c03fb0cc7b0d8d1417de7a4cc3
xrr2016 <13286904669@163.com>
2017-01-01 06:47:14 +0800

    :hankey: edit

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,25 @@
 <template>
-  <div id="app">
-    hi
-  </div>
+<div id="app">
+    <md-toolbar>
+        <h1 class="md-title">My Title</h1>
+    </md-toolbar>
+    <div class="main-content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo, rerum? Error sunt, aperiam dolores, atque expedita molestiae tenetur. Quis eveniet accusamus velit explicabo adipisci reiciendis modi eaque quas, officia excepturi.</p>
+    </div>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
-    return {
+    name: 'app',
+    data() {
+        return {}
     }
-  }
 }
 </script>
 
-<style >
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+<style lang="scss" scoped >
+.main-content {
+    padding: 16px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueMaterial from 'vue-material'
-import 'vue-material/dist/vue-material.css'
 
 Vue.use(VueMaterial)
 
