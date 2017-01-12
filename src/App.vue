8ff458ceb9ddcdb5bfe622a6c9d41afac67ba6cc
xrr2016 <13286904669@163.com>
2017-01-12 22:00:16 +0800

    :hankey:

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@
         <h1 class="md-title">My Title</h1>
     </md-toolbar>
     <div class="main-content">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo, rerum? Error sunt, aperiam dolores, atque expedita molestiae tenetur. Quis eveniet accusamus velit explicabo adipisci reiciendis modi eaque quas, officia excepturi.</p>
     </div>
 </div>
 </template>
