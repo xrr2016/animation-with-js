2f06da38150695953d94df7dc8f40eac5dffc27e
xrr2016 <xiaoranran1993@163.com>
2017-04-13 22:31:51 +0800

    :zap: state

--- public/app.jsx
+++ public/app.jsx
@@ -1,22 +1,39 @@
 const Greet = React.createClass({
+  getDefaultProps() {
+    return {
+      name: 'Any One!',
+      msg: 'This is a message.'
+    }
+  },
+  getInitialState() {
+    return {
+      name: this.props.name
+    }
+  },
+  handleSubmit(event) {
+    event.preventDefault()
+    const name = this.refs.name.value
+    this.refs.name.value = ''
+    this.setState({name})
+  },
   render() {
     return (
-      React.createElement(
-        'h2',
-        {style: {color: "blue"}},
-        "Happy Day!"
-      )
-      // <div>
-      //     <h1>Hello World!!!</h1>
-      // </div>
+      <div>
+          <h2>Hello {this.state.name}</h2>
+          <h3>{this.props.msg}</h3>
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" ref="name" />
+            <button>Set Name</button>
+          </form>
+      </div>
     )
   }
 })
 
+
 ReactDOM.render(
   <div>
-    <h1>Hello React!!!</h1>
-    <Greet />
+    <Greet msg="This is another message." />
   </div>,
   document.getElementById('app')
 )
