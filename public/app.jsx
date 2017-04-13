c36b37779d35433b8e198a0e80b51e50f91997de
xrr2016 <xiaoranran1993@163.com>
2017-04-13 22:02:02 +0800

    :fire: learning

--- public/app.jsx
+++ public/app.jsx
@@ -1,4 +1,22 @@
+const Greet = React.createClass({
+  render() {
+    return (
+      React.createElement(
+        'h2',
+        {style: {color: "blue"}},
+        "Happy Day!"
+      )
+      // <div>
+      //     <h1>Hello World!!!</h1>
+      // </div>
+    )
+  }
+})
+
 ReactDOM.render(
-  <h1>Hello React!!!</h1>,
+  <div>
+    <h1>Hello React!!!</h1>
+    <Greet />
+  </div>,
   document.getElementById('app')
 )
