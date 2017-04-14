1fb31ddc142bd0ff3b2daafa3fc7b5d3556270bd
xrr2016 <xiaoranran1993@163.com>
2017-04-14 23:21:25 +0800

    :bulb:

--- public/app.jsx
+++ public/app.jsx
@@ -1,29 +1,48 @@
 const Message = (props) => (
   <div>
-    <h2>{props.name}</h2>
+    <h2>Hello {props.name}!</h2>
     <p>{props.msg}</p>
   </div>
 )
 
 const Form = React.createClass({
-  handleSubmit(e) {
+  handleUpdate(e) {
     e.preventDefault()
+
+    const updates = {}
     const name = this.refs.name.value
+    const msg = this.refs.msg.value
+
+    if (name.length > 0 && msg.length > 0) {
+      this.refs.name.value = ''
+      this.refs.msg.value = ''
+      updates.name = name      
+      updates.msg = msg
+    }
+
+     if (msg.length > 0) {
+      this.refs.msg.value = ''
+      updates.msg = msg
+    }
+
     if (name.length > 0) {
       this.refs.name.value = ''
-      this.props.handleNewName(name)
-    }
+      updates.name = name
+    } 
+
+    this.props.onUpdates(updates)
   },
   render() {
     return (
-        <form onSubmit={this.handleSubmit}>
-          <input type="text" ref="name"/>
-          <button>Set Name</button>
+        <form onSubmit={this.handleUpdate}>
+          <input type="text" placeholder="enter name" ref="name"/>
+          <br/>
+          <textarea ref='msg' placeholder="enter message">{this.props.msg}</textarea> 
+          <p><button onClick={this.handleUpdate}>Update</button></p>
         </form>
     )
   }
 })
-
 
 const Greet = React.createClass({
   getDefaultProps() {
@@ -34,25 +53,24 @@ const Greet = React.createClass({
   },
   getInitialState() {
     return {
-      name: this.props.name
+      name: this.props.name,
+      msg: this.props.msg
     }
   },
-  handleNewName(name) {
-    this.setState({name})
+  handleUpdate(update) {
+    this.setState(update)
   },
   render() {
     return (
       <div>
-        <Message name={this.state.name} msg={this.props.msg}/>
-        <Form name={this.state.name} handleNewName={this.handleNewName}/>
+        <Message name={this.state.name} msg={this.state.msg}/>
+        <Form onUpdates={this.handleUpdate}/>
       </div>
     )
   }
 })
 
 ReactDOM.render(
-  <div>
-    <Greet msg="This is another message." />
-  </div>,
+  <Greet />,
   document.getElementById('app')
 )
