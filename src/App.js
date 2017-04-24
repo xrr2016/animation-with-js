f46e5669f4445cf7a321705b18bd923aaf155ed6
xrr2016 <xiaoranran1993@163.com>
2017-04-24 22:50:47 +0800

    :ok_hand:

--- src/App.js
+++ src/App.js
@@ -1,35 +1,57 @@
 import React, { Component } from 'react'
 // import logo from './logo.svg'
-import './App.css'
+// import './App.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootswatch/yeti/bootstrap.css'
+import { Navbar, NavItem, Nav, Grid, Row, Col } from 'react-bootstrap'
 import WeatherDisplay from './components/weatherDisplay'
 
 const Cities = [
-  { name: "Palo Alto", zip: "94303" },
-  { name: "San Jose", zip: "94088" },
-  { name: "Santa Cruz", zip: "95062" },
-  { name: "Honolulu", zip: "96803" }
+  { name: 'Palo Alto', zip: '94303' },
+  { name: 'San Jose', zip: '94088' },
+  { name: 'Santa Cruz', zip: '95062' },
+  { name: 'Honolulu', zip: '96803' }
 ]
-
 
 class App extends Component {
   state = {
     activeCity: 0
   }
 
-  render() {
+  render () {
     const activeCity = this.state.activeCity
     return (
-      <div className="App">
-        {Cities.map((city, index) => (
-          <button key={index}
-          onClick={() => this.setState({activeCity: index})}>
-            {city.name}
-          </button>
-        ))}
-        <WeatherDisplay key={activeCity} zip={Cities[activeCity].zip} />
+      <div>
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              Hello!
+            </Navbar.Brand>
+          </Navbar.Header>
+        </Navbar>
+        <Grid>
+          <Row>
+            <Col md={4} sm={4}>
+              <h3>Select a city</h3>
+              <Nav
+                bsStyle='pills'
+                stacked
+                activeKey={activeCity}
+                onSelect={index => this.setState({ activeCity: index })}
+              >
+                {Cities.map((city, index) => (
+                  <NavItem key={index} eventKey={index}>{city.name}</NavItem>
+                ))}
+              </Nav>
+            </Col>
+            <Col md={8} sm={8}>
+              <WeatherDisplay key={activeCity} zip={Cities[activeCity].zip} />
+            </Col>
+          </Row>
+        </Grid>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
