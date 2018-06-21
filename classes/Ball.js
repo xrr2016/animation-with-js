84db4fc9cbcb32ea03fccc336c5c2caa25744c49
xrr2016 <xiaoranran1993@163.com>
2018-06-21 23:05:55 +0800

    :apple: add more

---
+++ classes/Ball.js
@@ -0,0 +1,29 @@
+class Ball {
+  constructor(radius = 50, color = 'skyblue') {
+    this.radius = radius
+    this.color = color
+    this.x = 0
+    this.y = 0
+    this.rotation = 0
+    this.scaleX = 1
+    this.scaleY = 1
+    this.lineWidth = 1
+  }
+
+  draw(ctx) {
+    ctx.save()
+    ctx.translate(this.x, this.y)
+    ctx.rotate(this.rotation)
+    ctx.scale(this.scaleX, this.scaleY)
+    ctx.lineWidth = this.lineWidth
+    ctx.fillStyle = this.color
+    ctx.beginPath()
+    ctx.arc(0, 0, this.radius, 0, (Math.PI * 2), true)
+    ctx.closePath()
+    ctx.fill()
+    if (this.lineWidth > 0) {
+      ctx.stroke()
+    }
+    ctx.restore()
+  }
+}
